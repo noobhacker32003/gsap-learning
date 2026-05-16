3d18ad9d84b764a424fd77fd089c559af82d3a13
noobhacker32003 <asheq100mahmud@gmail.com>
2026-05-16 16:52:03 +0600

    string ready

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var initialPath = "M 10 100 Q 450 200 980 100";
-var finalPath = "M 10 100 Q 450 0 980 100";
+var finalPath = "M 10 100 Q 450 100 980 100";
 const string = document.querySelector("#string")
 // string.addEventListener("mouseenter", function () {
 //     gsap.to("#path", {
@@ -17,9 +17,19 @@ const string = document.querySelector("#string")
 // })  
 
 string.addEventListener("mousemove", function (dets) {
-    initialPath = `M 10 100 Q 450 ${dets.y} 980 100`
+    initialPath = `M 10 100 Q ${dets.x} ${dets.y} 980 100`
     gsap.to("#path", {
         attr: { d: initialPath },
+        duration: 0.3,
+        ease: "power3.out"
+
+    })
+})
+string.addEventListener("mouseleave", function (dets) {
+    gsap.to("#path", {
+        attr: { d: finalPath },
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)"
 
     })
 })
