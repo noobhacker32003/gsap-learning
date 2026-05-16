bae085fbc313eadbf9e34eaeeda060e10b865c52
noobhacker32003 <asheq100mahmud@gmail.com>
2026-05-16 16:37:07 +0600

    mouse moving string

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+var initialPath = "M 10 100 Q 450 200 980 100";
+var finalPath = "M 10 100 Q 450 0 980 100";
+const string = document.querySelector("#string")
+// string.addEventListener("mouseenter", function () {
+//     gsap.to("#path", {
+//         attr: { d: finalPath },
+//         duration: 1,
+//         ease: "power2.out"
+//     })
+// })
+// string.addEventListener("mouseleave", function () {
+//     gsap.to("#path", {
+//         attr: { d: initialPath },
+//         duration: 1,
+//         ease: "power2.out"
+//     })
+// })  
 
+string.addEventListener("mousemove", function (dets) {
+    initialPath = `M 10 100 Q 450 ${dets.y} 980 100`
+    gsap.to("#path", {
+        attr: { d: initialPath },
 
-gsap.to("#page2 h1", {
-    transform: "translateX(-100%)",
-    scrollTrigger: {
-        trigger: "#page2",
-        scroller: "body",
-        markers: true,
-        start: "top 0%",
-        end: "top -150%",
-        scrub: 2,
-        pin: true
-    }
-});
+    })
+})
